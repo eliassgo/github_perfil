afad5efa58d0b892a20f5d3787abcdacb59d6e42
Elias Gomes <eliasgo221@gmail.com>
2023-07-06 10:57:32 -0300

    adicionando tratativa de erros

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,23 @@
 // Primeiro: importacao
+// OnBlur: Evento que ocorre quando o campo de entrada perde o foco. Quando o campo perde o foco é acionado a função de mudar o NomeUsuario para o valor que está no input 
 import { useState } from "react";
 
 import Perfil from "./components/Perfil";
-import Formulario from "./components/Formulario";
 import ReposList from "./components/ReposList";
+
 
 function App() {
   const [formularioEstaVisivel, setFormularioEstaVisivel] = useState(true);
   const [nomeUsuario, setNomeUsuario] = useState('');
   return (
     <>
-      <input type="text" onBlur={(e) => setNomeUsuario(e.target.value)} />
+      <div className="formulario">
+        <h1>Perfil GitHub: Busque seus repositórios</h1>
+        <form action="">
+          <label htmlFor="usuario">Usuário</label>
+          <input type="text" id="usuario" onBlur={(e) => setNomeUsuario(e.target.value)} />
+        </form>
+      </div>
       {nomeUsuario.length > 4 && (
         <>
           <Perfil nomeUsuario={nomeUsuario} />
--- src/components/ReposList/index.jsx
+++ src/components/ReposList/index.jsx
@@ -1,26 +1,38 @@
 import { useEffect, useState } from "react"
 import styles from './RespoList.module.css'
-
+/* 
+useState: hook que permite adicionar estado a um componente funcional. 
+Consiste em um par de valores, uma variável de estado e uma função para atualizar o estado. 
+useState permite adicionar estado a um componente funcional
+useEffect permite executar efeitos colaterais em momentos específicos do ciclo de vida do componente
+set... são usadas para atualizar o estado de um componente, refletindo essas alterações na renderização.
+*/
 const ReposList = ({ nomeUsuario }) => {
-    const [repos, setRepos] = useState([]);
-    const [estaCarregando, setEstaCarregando] = useState([]);
-
-    useEffect(() => {
+    const [repos, setRepos] = useState([]); // Estado repos que como um array vazio para armazenar os repositório obtidos
+    const [estaCarregando, setEstaCarregando] = useState([false]); // 
+    const [deuErro, setDeuErro] = useState(false)
+    useEffect(() => { // hook que é executado sempre que há uma alteração no valor nomeUsuario
         setEstaCarregando(true);
-        fetch(`https://api.github.com/users/${nomeUsuario}/repos`)
-            .then(res => res.json())
+        fetch(`https://api.github.com/users/${nomeUsuario}/repos`) // Chamada à APi para obter os repos
+            .then(res => res.json()) // Convertendo a resposta em json 
             .then(resJon => {
-                setTimeout(() => {
-                    setEstaCarregando(false);
-                    setRepos(resJon);
+                setTimeout(() => { // Definição do timeout para simular um atraso no carregamento 
+                    setEstaCarregando(false); // Indicando que o carregamento foi concluído 
+                    setRepos(resJon); // Atualizando a const repos com os dados obtidos 
                 }, 3000);
+            })
+            .catch(e => {
+                setDeuErro(true)
+                setEstaCarregando(false);
             })
     }, [nomeUsuario]); // Quando houver uma mudança no nome de usuário o useEffect vai executar e fazer a requisição
 
     return (
         <div className="container">
-            {estaCarregando ? (
-                <h1>Carregando...</h1>
+            {deuErro ? (
+                <h1>Usuário não encontrado. Verifique o seu nome de usuário</h1>
+            ) : estaCarregando ? ( // Verificação se estaCarregando é verdadeira, se sim mostra o carregando.. 
+                <h1>Buscando os dados...</h1>
             ) : (
                 <u className={styles.list}>
                     {repos.map(({ id, name, language, html_url }) => (
@@ -39,7 +51,6 @@ const ReposList = ({ nomeUsuario }) => {
                     ))}
                 </u>
             )}
-
         </div>
     )
 }
